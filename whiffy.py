976f6da9042ef4a9b9747ee9d10d1a00339b3059
Grahame Bowland <grahame@angrygoats.net>
2012-12-10 11:52:18 +0800

    improve confidence by plotting what it is doing!

--- whiffy.py
+++ whiffy.py
@@ -4,15 +4,30 @@
 # Whiffy, a WFS downloader (currently works against Geoserver WFS)
 #
 
-import sys, collections, urllib.request, urllib.parse, json
+import sys, collections, urllib.request, urllib.parse, json, sys
 from functioncache import cache_result
 from pprint import pprint
 
 LatLng = collections.namedtuple('LatLng', ('lat', 'lng'))
 BBox = collections.namedtuple('BBox', ('ne', 'sw'))
 
+def plot(bounds, done):
+    print("plotting recursion")
+    import matplotlib.pylab as pylab
+    ax = pylab.gca()
+    for bbox in done:
+        x = bbox.sw.lng
+        y = bbox.sw.lat
+        h = bbox.ne.lat - bbox.sw.lat
+        w = bbox.ne.lng - bbox.sw.lng
+        ax.add_patch(pylab.Rectangle((x,y), w, h, fill=False))
+    ax.set_xlim(bounds.sw.lng, bounds.ne.lng)
+    ax.set_ylim(bounds.sw.lat, bounds.ne.lat)
+    pylab.show()
+
 def retrieve_uri(uri):
-    sys.stderr.write("retrieving: %s\n" % uri)
+    sys.stderr.write(" (get: %s) " % uri)
+    sys.stderr.flush()
     req = urllib.request.Request(uri)
     req.add_header('User-Agent', 'potd.py')
     return urllib.request.urlopen(req)
@@ -48,33 +63,38 @@ class WfsWrapper:
             qparts.append(q)
         return ''.join(parts) + '&'.join(qparts)
 
-    def get_everything(self, bbox, acceptance_fn):
+    def get_everything(self, bounds, acceptance_fn):
         def quad_split(bbox):
             h = bbox.ne.lat - bbox.sw.lat
             w = bbox.ne.lng - bbox.sw.lng
-            return [ BBox(bbox.sw, LatLng(bbox.sw.lat + h/2, bbox.sw.lng + w/2)), 
-                     BBox(LatLng(bbox.sw.lat, bbox.sw.lng + w/2), LatLng(bbox.sw.lat + h/2, bbox.ne.lng)), 
-                     BBox(LatLng(bbox.sw.lat + h/2, bbox.sw.lng), LatLng(bbox.ne.lat, bbox.sw.lng + w/2)), 
-                     BBox(LatLng(bbox.sw.lat + h/2, bbox.sw.lng + w/2), LatLng(bbox.ne.lat, bbox.ne.lng)) ]
+            return [ BBox(sw=bbox.sw, ne=LatLng(bbox.sw.lat + h/2, bbox.sw.lng + w/2)), 
+                     BBox(sw=LatLng(bbox.sw.lat, bbox.sw.lng + w/2), ne=LatLng(bbox.sw.lat + h/2, bbox.ne.lng)), 
+                     BBox(sw=LatLng(bbox.sw.lat + h/2, bbox.sw.lng), ne=LatLng(bbox.ne.lat, bbox.sw.lng + w/2)), 
+                     BBox(sw=LatLng(bbox.sw.lat + h/2, bbox.sw.lng + w/2), ne=LatLng(bbox.ne.lat, bbox.ne.lng)) ]
 
-        bbox_data_ok = {}
-        queue = [bbox]
+        accepted = {}
+        queue = [(0, bounds)]
+        depth = 0
         while queue:
             pending = []
-            pprint(queue)
-            for bbox in queue:
-                print("GET", bbox)
+            for i, (depth, bbox) in enumerate(queue):
+                sys.stderr.write("[%d/%d @ %d :%d] %s" % (i, len(queue), depth, len(pending), str(bbox)))
+                sys.stderr.flush()
                 json_data = get_json_data(bbox)
                 try:
                     geom_data = json.loads(json_data.decode('utf8'))
                 except ValueError:
-                    print("Invalid data:", json_data)
+                    sys.stderr.write("Invalid data:", json_data)
+                    sys.stderr.flush()
                     raise
+                sys.stderr.write(" -> %d\n" % (len(geom_data['features'])))
+                sys.stderr.flush()
                 if acceptance_fn(geom_data):
-                    bbox_data_ok[bbox] = geom_data
+                    accepted[bbox] = geom_data
                 else:
-                    pending += quad_split(bbox)
+                    pending += [(depth+1, t) for t in quad_split(bbox)]
             queue = pending
+        plot(bounds, list(accepted.keys()))
 
 if __name__ == '__main__':
     wrapper = WfsWrapper('https://www2.landgate.wa.gov.au/ows/wfspublic_4283/wfs', 'WCORP-001')
